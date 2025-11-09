208983daf7741448bfe4f1aff11442fcfe047eff
roshanpatil2000 <roshanpatil2912.rp@gmail.com>
2025-11-09 19:57:04 +0530

    updated version

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.static('public'));
 
 // Base health check route
 app.get("/", (req, res) => {
-    res.json({ version: "0.0.4", releaseDate: "08-nov-2025", message: "CAFÈ API is running 🚀" });
+    res.json({ version: "0.0.5", releaseDate: "09-nov-2025", message: "CAFÈ API is running 🚀" });
 });
 
 // routes
